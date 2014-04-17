b6620a36285318ac16e3e1ead0cf17a57e8416f8
r02922010 <r02922010@linux19.csie.ntu.edu.tw>
2014-04-17 13:58:40 +0800

    update utility

--- python/networkx/graph_utility.py
+++ python/networkx/graph_utility.py
@@ -2,8 +2,7 @@
 import networkx as nx
 
 def print_graph_info(g, show_data=False):
-    print("Number of nodes: " + g.number_of_nodes())
-    print("Nodes: " + g.nodes(data = show_data))
-    print("Number of edges: " + g.number_of_edges())
-    print("Edges: " + g.edges(data = show_data))
-
+    print('--------------start---------------')
+    print("Number of nodes: ", g.number_of_nodes(), "Nodes: ", g.nodes(data = show_data))
+    print("Number of edges: ", g.number_of_edges(), "Edges: ", g.edges(data = show_data))
+    print('---------------end----------------')
