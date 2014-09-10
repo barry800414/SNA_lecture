de0b0959e50e7bde9cf935dc076fe924da5f4bdf
r02922010 <r02922010@cluster.aracluster>
2014-09-10 19:06:47 +0800

    update

--- networkx/link_prediction/run.bat
+++ networkx/link_prediction/run.bat
@@ -1,4 +1,4 @@
 python .\link_prediction.py ..\..\gen_pokec_data\train.csv ..\..\gen_pokec_data\test.csv ..\..\gen_pokec_data\test.ans ..\..\gen_pokec_data\pre_nodes_profile.csv config.json train.dat test.dat
-..\..\liblinear-1.94\windows\train.exe train.dat
-..\..\liblinear-1.94\windows\predict.exe test.dat train.dat.model test.predict
+..\liblinear-1.94\windows\train.exe train.dat
+..\liblinear-1.94\windows\predict.exe test.dat train.dat.model test.predict
 pause
