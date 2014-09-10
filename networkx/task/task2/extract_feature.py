8111e1286e041466e68015d42d400a315531c7b8
ming <barry800414@gmail.com>
2014-09-10 21:20:46 +0800

    update

--- networkx/task/task2/extract_feature.py
+++ networkx/task/task2/extract_feature.py
@@ -22,6 +22,7 @@ pairs: the pairs of nodes to extract features
 
 NO_PATH_LENGTH = 1000
 
+
 # get all edge embeddedness(number of common neighbors) for the edge in edges
 def get_edge_embeddedness(graph, pairs):
     c = Column(1, 'numerical')
@@ -29,24 +30,27 @@ def get_edge_embeddedness(graph, pairs):
     for pair in pairs:
         n1 = pair[0]
         n2 = pair[1]
-        value[pair] = len(list(nx.common_neighbors(graph, n1, n2)))
+        # TODO Task_2: EDIT HERE, please find the number of 
+        # common neighbors of n1 and n2
+        # value[pair] = ooxx
     c.value = value
     return c
 
-# get shortest path length for all edges
-def get_shortest_path_length(graph, pairs):
+# get all jaccards_coefficent for all pairs
+def get_jaccards_coefficient(graph, pairs):
     c = Column(1, 'numerical')
     value = dict()
     for pair in pairs:
         n1 = pair[0]
         n2 = pair[1]
-        try:
-            # TODO Task_2: EDIT HERE, please find the number of 
-            # common neighbors of n1 and n2
-            # value[pair] = ooxx
-            pass
-        except:
-            value[pair] = NO_PATH_LENGTH
+        nei_x = set(graph.neighbors(n1))
+        nei_y = set(graph.neighbors(n2))
+        if len(nei_x) == 0 or len(nei_y) == 0:
+            value[pair] = 0.0
+        else:
+            union = nei_x | nei_y
+            intersect = nei_x & nei_y
+            value[pair] = float(len(intersect)) / len(union)
     c.value = value
     return c
 
