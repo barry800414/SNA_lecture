de22f3f8c947058ef470d913f2b151ce0896999b
r02922010 <r02922010@cluster.aracluster>
2014-09-10 17:41:03 +0800

    update

--- networkx/link_prediction/run.sh
+++ networkx/link_prediction/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 path=../gen_pokec_data
 exec_path=../liblinear-1.94
-./link_prediction.py $path/train.csv $path/test.csv $path/test.ans $path/pre_nodes_profile.csv config.json train.dat test.dat
+python link_prediction.py $path/train.csv $path/test.csv $path/test.ans $path/pre_nodes_profile.csv config.json train.dat test.dat
 $exec_path/train train.dat
 $exec_path/predict test.dat train.dat.model test.predict
---
+++ networkx/prepare.sh
@@ -0,0 +1,2 @@
+cd ./liblinear-1.94/
+make
